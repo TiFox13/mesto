69440911ac59c8c3a87fb68d8da4394037d3308f
TiFox13 <mozi.rin@yandex.ru>
2022-09-08 13:06:29 +0300

    fix: Исправить подключение шрифта и выправить разметку

--- index.js
+++ index.js
@@ -1,44 +1,36 @@
-let editButton = document.querySelector('.edit-button');
-let popup = document.querySelector('.popup');
-function showClick() {
-    popup.classList.add('popup_opened');
-}
-editButton.addEventListener('click', showClick);
+let editButton = document.querySelector('.edit-button'); //кнопка "редактировать"
+let closeButton = document.querySelector('.close-button'); //кнопка "закрыть форму"
+let popup = document.querySelector('.popup'); //всплывающее окошко с формой
 
-
-let closeButton = document.querySelector('.close-button');
-function closeClick() {
-    popup.classList.remove('popup_opened'); 
-}
-closeButton.addEventListener('click', closeClick);
-
-
-
-//let saveButton = document.querySelector('.save-button');
 let profileName = document.querySelector('.profile__name'); //вот переменная, куда загрузим имя
 let profileAbout = document.querySelector('.profile__about'); // вот сюда мы загрудим остальную инфу
 
 let formElement = document.querySelector('.form');  //вот переменная с формой
-
 let nameInput = formElement.querySelector('.name'); //поле формы с именем
 let jobInput = formElement.querySelector('.about'); //поле формы с доп инфой
 
+
+//функция открытия формы
+function showClick() {
+    popup.classList.add('popup_opened');
+}
+
+//функция закрытия формы
+function closeClick() {
+    popup.classList.remove('popup_opened'); 
+}
+
+//функция сохранения изменений в форме
 function formSubmitHandler (evt) {
     evt.preventDefault(); //отключили стандартную отправку формы
     profileName.textContent = nameInput.value; // сказали "запиши мне в ProfileName содержимое поля с именем"
     profileAbout.textContent = jobInput.value; // то же самое для остальной инфы
     popup.classList.remove('popup_opened'); // "закрой форму"
 }
-//saveButton.addEventListener('click', formSubmitHandler)
+
+
+editButton.addEventListener('click', showClick);
+closeButton.addEventListener('click', closeClick);
 formElement.addEventListener('submit', formSubmitHandler); //при событии "отправка" запускаем функцию редактирования данных
 
 
-
-
-let like = document.querySelector('.like');
-/*функция включает и выключает лайк у первой карточки. просто посмотреть, работает ли при изменении свойства 
-background-image.  Дя! работает \^-^/  */
-function likeClick() {
-    like.classList.toggle('like_active');
-    }
-like.addEventListener('click', likeClick);  
