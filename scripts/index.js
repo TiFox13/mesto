6b5ec4655a9d9179d4a3423564287a546dc0a6a3
TiFox13 <mozi.rin@yandex.ru>
2022-09-21 18:31:58 +0300

    feat: реализовать добавление карточек через  форму JS, настроить плавное появление попапов

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,4 @@
 
-
-
 let editButton = document.querySelector('.edit-button'); //кнопка "редактировать"
 let closeButtons = document.querySelectorAll('.close-button'); //кнопка "закрыть форму"
 let popups = document.querySelectorAll('.popup'); //всплывающее окошко с формой
@@ -14,34 +12,14 @@ let newPlaseForm = document.querySelector('.new-plase-form');
 let nameInput = formElement.querySelector('.form__item_content_name'); //поле формы с именем
 let jobInput = formElement.querySelector('.form__item_content_about'); //поле формы с доп инфой
 
+let plaseName = document.querySelector ('.form__item_content_plase-name');  //поле формы для нового места
+let plaseImage = document.querySelector('.form__item_content_plase-image');  // поле формы для нового места
 
-//функция открытия формы
-function showClick(index) {
-    nameInput.value = profileName.textContent; // подгрузили в поля формы нужное имя
-    jobInput.value = profileAbout.textContent; // то же самое для остальной инфы
-    popups[index].classList.add('popup_opened');
-   }
+let cardMaket = document.querySelector('template#card'); //болванка для карточек
+let element = cardMaket.content; //содержимое болванки
+let addButton = document.querySelector('.add-button');  // Это кнопка добавления нового места
 
-//функция закрытия формы
-function closeClick(index) {
-    popups[index].classList.remove('popup_opened');
-}
-
-//функция сохранения изменений в форме
-function formSubmitHandler (evt) {
-    evt.preventDefault(); //отключили стандартную отправку формы
-    profileName.textContent = nameInput.value; // сказали "запиши мне в ProfileName содержимое поля с именем"
-    profileAbout.textContent = jobInput.value; // то же самое для остальной инфы
-    closeClick(0); // вызвали функцию закрытия формы
-}
-
-
-
-
-
-//ПРОЕКТНАЯ РАБОТА 5
-
-
+let popapImg = document.querySelector('.popup-image');
 
 const initialCards = [
   {
@@ -70,9 +48,30 @@ const initialCards = [
   }
 ];
 
-let cardMaket = document.querySelector('template#card');
-let element = cardMaket.content;
-let elements = document.querySelector('.elements');
+
+//функция открытия формы
+function showClick(index) {
+    nameInput.value = profileName.textContent; // подгрузили в поля формы нужное имя
+    jobInput.value = profileAbout.textContent; // то же самое для остальной инфы
+    popups[index].classList.add('popup_opened');
+   }
+
+//функция закрытия формы
+function closeClick(index) {
+    popups[index].classList.remove('popup_opened');
+}
+
+//функция сохранения изменений в форме
+function formSubmitHandler (evt) {
+    evt.preventDefault(); //отключили стандартную отправку формы
+    profileName.textContent = nameInput.value; // сказали "запиши мне в ProfileName содержимое поля с именем"
+    profileAbout.textContent = jobInput.value; // то же самое для остальной инфы
+    closeClick(0); // вызвали функцию закрытия формы
+}
+
+
+
+//Функция, кторая делает карточки
 function createCard(item) {
   let card = element.cloneNode(true);
   let text = card.querySelector('.element__text');
@@ -80,43 +79,80 @@ function createCard(item) {
 
   text.textContent = item.name;
   image.src = item.link;
-
-  elements.append(card);
+  image.alt = item.name;
+  setListeners(card);
 
   return card;
 };
 
-for (let i = 0; i < initialCards.length; i = i + 1) {
-createCard(initialCards[i]);
+// функция, которая навешивает леща(простите, слушатели навешивает) и выводит стартовые карточки на страницу
+function cardPlaserOld(item) {
+  let elements = document.querySelector('.elements');
+  const card =createCard(item);
+  elements.append(card);
+}
+// функция, которая также навешивает слушатели и выводит на страницу, но уже НОВЫЕ карточки, которые добавит пользователь
+function cardPlaserNew(item) {
+  let elements = document.querySelector('.elements');
+  const card =createCard(item);
+  elements.prepend(card);
+}
 
+//листалка для массива, запрашивает создание и выведение на экран стартовых карточек
+for (let i = 0; i < initialCards.length; i = i + 1) {  
+  cardPlaserOld(initialCards[i]);
 }
 
 
 
-
-let addButton = document.querySelector('.add-button');  // Это кнопка добавления нового места
-
-let name = document.querySelector('.form__item_content_plase-name');
-let link = document.querySelector('.form__item_content_plase-image');
-
+//отправка формы для создания новой карточки
 function newPlaiseCreate (evt) {
   evt.preventDefault(); //отключили стандартную отправку формы
-  const newPlase = {plaseName, plaseImage};
-  createCard(newPlase);
-  closeClick(1); // вызвали функцию закрытия формы
+  let name = document.querySelector('.form__item_content_plase-name').value;  //забираем из поля формы название
+  let link = document.querySelector('.form__item_content_plase-image').value; // забираем из поля формы адрес картинки
+  const newPlase = {name, link}; //создаем массив
+  initialCards.unshift(newPlase); //делаем его элементом стартового массива ( навсякий случай)
+  cardPlaserNew(newPlase);
+  plaseName.value = '';  //очистили поле
+  plaseImage.value =''; //очистили поле
+  closeClick(1); // вызвали функцию закрытия этой формы
 }
 
-function setListeners () {
-card.querySelector('.like').addEventListener('click', function (evt) {
-  evt.target.classList.toggle('like_active');});
+
+
+
+//это функция с кнопочками на карточках
+function setListeners (element) {           //ЭТА ФУНКЦИЯ РАБОТАЕТ                                
+//функция любви. отвечает за лайки
+  element.querySelector('.like').addEventListener('click', function (evt) {   
+    evt.target.classList.toggle('like_active');});
+    
+//функция, которая удаляет карточки
+  element.querySelector('.trash-button').addEventListener('click', function(evt) {      //И ЭТА ТОЖЕ
+    let del = evt.target.closest('.element');
+    del.remove();
+});
+
+
+//функция плейс-попап
+ element.querySelector ('.element__photo').addEventListener('click', function(evt) {
+ let plaseImg= popapImg.querySelector('.popup-image__image');
+let plaseInfo= popapImg.querySelector('.popup-image__plase-info');
+plaseImg.src = evt.target.src;  //ЗАРАБОТАЛО БЛЯТЬ!
+plaseInfo.textContent = evt.target.alt;
+//plaseInfo.textContent = text; //не работает
+
+  popapImg.classList.add('popup_opened');
+ })
 }
 
 
-editButton.addEventListener('click', ()=> showClick(0)); // Открытие первого окна
-addButton.addEventListener('click', ()=> showClick(1)); // Открытие второго окна
-closeButtons[0].addEventListener('click', ()=> closeClick(0));
-closeButtons[1].addEventListener('click', ()=> closeClick(1))
+editButton.addEventListener('click', ()=> showClick(0)); // Открытие первого окна(редактирование профиля)
+addButton.addEventListener('click', ()=> showClick(1)); // Открытие второго окна (создание карточек)
+closeButtons[0].addEventListener('click', ()=> closeClick(0)); // Закрытие первого окна
+closeButtons[1].addEventListener('click', ()=> closeClick(1)) // Закрытие второго окна
+closeButtons[2].addEventListener('click', ()=> closeClick(2)) //закрытие окна с увеличенной картинкой                                                                     
 formElement.addEventListener('submit', formSubmitHandler); //при событии "отправка" запускаем функцию редактирования данных
-newPlaseForm.addEventListener('submit', newPlaiseCreate);
+newPlaseForm.addEventListener('submit', newPlaiseCreate); // при событии "отправка" создаем новую карточку
 
 
